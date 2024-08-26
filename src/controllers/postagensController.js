ee82ec8b6570dba4ee4b825443805e4cb664a008
Thug-Okami <m.senna.cordeiro@gmail.com>
2024-08-26 08:57:15 -0300

    correção de sintaxe

--- src/controllers/postagensController.js
+++ src/controllers/postagensController.js
@@ -2,9 +2,9 @@ const dbConnection = require('../models/dbConnection');
 const express = require('express');
 const router = express.Router();
 
-router.get('/id/:id' (req,res) => {
+router.get('/id/:id', (req,res) => {
     const id = req.params.id;
-    const query ='select * from Postagens where id = ?'
+    const query ='select * from Postagens where id = ?';
     dbConnection.query(query, [id], (err, results) => {
         if(err) throw err;
         res.json(results);
