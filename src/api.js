37415af189e78bbbb3743f4847d4a3b8c8868a74
BlackKing72 <matheus.csilv14@gmail.com>
2024-09-29 17:12:18 -0300

    Testando deploy com netlify

--- src/api.js
+++ src/api.js
@@ -5,33 +5,34 @@ const cors = require('cors');
 
 const owlpostRouter = require('./routes/owlpostRouter');
 
-const app = express();
+const api = express();
 const port = 3000;
 
-app.use(cors({
+api.use(cors({
     origin: '*',
     methods: ['POST', 'PUT', 'PATCH', 'DELETE', 'GET'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
 }));
 
-app.use(express.json({
+api.use(express.json({
     limits: '40mb'
 }));
 
-app.use(expressFileUpload({
+api.use(expressFileUpload({
     limits: {
         fileSize: 40 * 1024 * 1024 // Limite de 40MB
     }
 }));
 
-app.use('/', owlpostRouter);
+api.use('/', owlpostRouter);
 
-app.get('/', (req, res) => res.send("Express on Vercel"));
+api.get('/', (req, res) => res.send("Express on Vercel"));
 
-app.listen(port, () => {
-    console.log(`Servidor rodando na porta: ${port}`);
-});
+// app.listen(port, () => {
+//     console.log(`Servidor rodando na porta: ${port}`);
+// });
 
-module.exports = app;
-module.exports.handler = serverless(app);
+// module.exports = app;
+// module.exports.handler = serverless(app);
+export const handler = serverless(api);
