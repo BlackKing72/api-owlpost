b8b8f28fcdb0670e9c8758a84e4112981de77cbd
BlackKing72 <matheus.csilv14@gmail.com>
2024-09-30 08:10:34 -0300

    Testando nova config do vercel

--- src/models/dbConnection.js
+++ src/models/dbConnection.js
@@ -1,11 +1,17 @@
 const mysql = require('mysql');
-const database = 'owlpost';
+
+const database = process.env.DB_NAME || 'owlpost';
 
 const dbConnection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: database
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: database,
+    ssl: {
+        rejectUnauthorized: true,
+        ca: process.env.DB_SSL || fs.readFileSync(__dirname + '../../../ca.pem')
+    }
 });
 
 dbConnection.connect((err) => {
