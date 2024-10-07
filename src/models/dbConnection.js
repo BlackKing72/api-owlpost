d5b4e5ffb12e08100d0e378afcff973f53831fab
BlackKing72 <matheus.csilv14@gmail.com>
2024-10-07 18:19:25 -0300

    Aparentemente funcionando 100%

--- src/models/dbConnection.js
+++ src/models/dbConnection.js
@@ -1,23 +1,37 @@
 const mysql = require('mysql2');
+const fs = require('fs');
 
 const database = process.env.DB_NAME || 'owlpost';
 
-const dbConnection = mysql.createConnection({
-    host: process.env.DB_HOST || 'localhost',
-    port: process.env.DB_PORT,
-    user: process.env.DB_USER || 'root',
-    password: process.env.DB_PASS || '',
-    database: database,
-    ssl: {
-        rejectUnauthorized: true,
-        ca: process.env.DB_SSL || fs.readFileSync(__dirname + '../../../ca.pem')
-    }
-});
+let pool;
 
-dbConnection.connect((err) => {
-    err
-        ? console.error(`Erro ao tentar conectar ao banco de dados ${database}`)
-        : console.log(`Banco ${database} conectado com sucesso!`);
-});
+const getPool = () => {
+    if (!pool) {
+        pool = mysql.createPool({
+            host: process.env.DB_HOST || 'localhost',
+            port: process.env.DB_PORT,
+            user: process.env.DB_USER || 'root',
+            password: process.env.DB_PASS || '',
+            database: database,
+            waitForConnections: true,
+            connectionLimit: 10,
+            queueLimit: 0,
+            ssl: {
+                rejectUnauthorized: true,
+                ca: process.env.DB_SSL || fs.readFileSync(__dirname + '../../../ca.pem')
+            }
+        });
+    }
+
+    return pool;
+}
+
+const dbConnection = getPool();
+
+// dbConnection.connect((err) => {
+//     err
+//         ? console.error(`Erro ao tentar conectar ao banco de dados ${database}`)
+//         : console.log(`Banco ${database} conectado com sucesso!`);
+// });
 
 module.exports = dbConnection;
